bd683eed04ee707f35511ae9b5179ee5ffa1ff3b
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2020-04-21 07:20:35 -0300

    Initial

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,13 @@
+const connection = require("../database/connection");
+
+module.exports = {
+  async login(request, response) {
+    const { id } = request.body;
+
+    const ong = await connection("ongs").where("id", id).select("name").first();
+
+    if (!ong) return response.status(400).json({ error: "Invalid Code." });
+
+    return response.json(ong);
+  },
+};
