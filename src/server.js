a91b80003e7aacba395d2da37ea65974190d3b5f
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2020-04-27 13:16:53 -0300

    Fixes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const colors = require("colors");
 const app = require("./app");
 
-const port = process.env.SERVER_PORT ? process.env.SERVER_PORT : 3333;
+const port = process.env.PORT ? process.env.PORT : 3000;
 
 console.log(
   "\nServidor iniciado em: " + colors.cyan(`http://localhost:${port}/`)
